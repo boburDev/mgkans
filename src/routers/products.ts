cdb0fd4ccff70824151f76f067fc2158c5f662fc
boburDev <negmatov1907@gmail.com>
2024-11-25 23:32:08 +0500

    Update products.ts

--- src/routers/products.ts
+++ src/routers/products.ts
@@ -9,7 +9,7 @@ router
     .get('/:name/all', products.getProducts)
     .get('/signle/:id', products.getSingleProduct)
     .post('/create', uploadPhoto.array('photos', 5), products.createProduct)
-    .delete("/delete", products.deleteProduct);
+    .post('/delete', products.deleteProduct);
 
 
 export default router;
