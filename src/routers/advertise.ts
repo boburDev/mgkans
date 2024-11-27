984e19a40b32ce560e5244815ea7d1693a7c430b
boburDev <negmatov1907@gmail.com>
2024-11-27 22:21:19 +0500

    Update advertise.ts

--- src/routers/advertise.ts
+++ src/routers/advertise.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import * as advertise from '../controllers/advertise';
+import { uploadPhoto } from '../middlewares/multer'
 
 const router = express.Router();
 
 router
     .get('/all', advertise.getAds)
-    .post('/create', advertise.createAd)
+    .post('/create', uploadPhoto.single('ads'), advertise.createAd)
 
 export default router;
