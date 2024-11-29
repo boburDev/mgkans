53ff9adefb235c8f56458b54fb90a1526fe1fdf0
boburDev <negmatov1907@gmail.com>
2024-11-29 20:15:04 +0500

    Update advertiseController.ts

--- src/controllers/advertiseController.ts
+++ src/controllers/advertiseController.ts
@@ -7,7 +7,7 @@ import Category from '../models/category';
 export const getAdsByCategory = async (req: Request, res: Response) => {
     try {
         console.log(req.params.name);
-        const catalog = await Category.findOne({ path: `/${req.params.name}` });
+        const catalog = await Category.findOne({ route: `/${req.params.name}` });
         console.log(catalog);
         
         let ads
