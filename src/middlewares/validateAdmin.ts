b78787c258419d3d782852903742c9371211f19a
boburDev <negmatov1907@gmail.com>
2024-11-29 19:05:46 +0500

    Update validateAdmin.ts

--- src/middlewares/validateAdmin.ts
+++ src/middlewares/validateAdmin.ts
@@ -17,9 +17,12 @@ export const validateJWT = async (req: Request, res: Response, next: NextFunctio
 
     try {
         let decoded: Admin | null = verifyAdmin(String(token));
+        console.log(decoded);
         if (!decoded) throw new Error("Invalid token!");
-
+        
         const user = await User.findById(decoded.id);
+        console.log(user)
+        
         if (!user) throw new Error('Admin not found!');
 
         req.admin = decoded
