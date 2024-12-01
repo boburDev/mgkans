174e722781c67bdded50d1f24c8818efd90ab205
boburDev <negmatov1907@gmail.com>
2024-12-01 17:54:53 +0500

    Update comments.ts

--- src/routers/comments.ts
+++ src/routers/comments.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import * as comment from '../controllers/commentController';
 import { validateJWT } from '../middlewares/validateAdmin';
+import { authMiddleware } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
 router
     .get('/all', validateJWT, comment.getAllComments)
     .get('/:productId', comment.getCommentsByProduct)
-    .post("/create", comment.createProductComment)
+    .post("/create", authMiddleware, comment.createProductComment)
     .post("/delete", validateJWT, comment.deleteProductComment)
 
 export default router;
