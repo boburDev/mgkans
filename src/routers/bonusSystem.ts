cb0cddf0995e37c0aad64ae3f2ad694f2e270442
boburDev <negmatov1907@gmail.com>
2024-12-03 22:44:06 +0500

    Update bonusSystem.ts

--- src/routers/bonusSystem.ts
+++ src/routers/bonusSystem.ts
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router
     .get('/all', validateJWT, bonusSystem.getAllBonusSystems)
-    .get('/bonus', authMiddleware, bonusSystem.getBonusSystemByToken)
+    .get('/personal', authMiddleware, bonusSystem.getBonusSystemByToken)
     .post('/create', authMiddleware, uploadPhoto.single('bonus'), bonusSystem.createBonusSystem)
     .post('/delete/:id', validateJWT, bonusSystem.deleteBonusSystem)
 
