dd9bba34382993fa50611b220c88994cf37230e4
boburDev <negmatov1907@gmail.com>
2024-12-17 22:02:47 +0500

    Update orders.ts

--- src/routers/orders.ts
+++ src/routers/orders.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router
     // .get('/all', orders.getAllOrders) // Fetch all orders with optional filters
-    .get('/:userId', authMiddleware, orders.getUserOrders) // Fetch orders for a specific user
+    .get('/list', authMiddleware, orders.getUserOrders) // Fetch orders for a specific user
     .post('/create', authMiddleware, orders.createOrder); // Create a new order
 
 export default router;
