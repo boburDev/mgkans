461cf519db5b1e92648d999428333d037a1180c3
boburDev <negmatov1907@gmail.com>
2024-12-17 21:32:39 +0500

    Update contractController.ts

--- src/controllers/contractController.ts
+++ src/controllers/contractController.ts
@@ -92,7 +92,7 @@ export const getContractSingle = async (req: Request, res: Response): Promise<an
     try {
         if (!req?.user?.isLegal) throw new Error("User is not legal");
         const id = req?.user?.userLegal?.conterAgentId
-        console.log(id)
+        console.log(id, req?.user)
         
         if (!id) {
             res.status(200).json({ data: null });
